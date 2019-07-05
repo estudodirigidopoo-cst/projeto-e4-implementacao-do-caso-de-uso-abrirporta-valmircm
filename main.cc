ce1ee2316ccad00261a76970b43e85667213c3b8
valmircm <valmir.cm@aluno.ifsc.edu.br>
2019-07-05 08:03:57 -0300

    Add main

--- main.cc
+++ main.cc
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include "interfacePorta.h"
 
 using namespace std;
 
-int main(void)
-{
+InterfacePorta porta1;
+
+int main(void) {
+    
+    bool flag;
+    porta1.abrirPorta(123456);
     
 }
