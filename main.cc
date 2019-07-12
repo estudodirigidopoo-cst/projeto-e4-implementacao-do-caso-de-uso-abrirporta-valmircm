5c642e5ef56df1785f67dcbb07f18bb0e6393630
valmircm <valmir.cm@aluno.ifsc.edu.br>
2019-07-12 09:02:33 -0300

    Com mais de uma interface

--- main.cc
+++ main.cc
@@ -9,13 +9,24 @@ using namespace std;
 int main(void) {
 
     InterfacePorta *interface1 = new InterfacePorta("Porta1-Redes1", "Redes1");
-    ControladorAcesso controlador;
+    InterfacePorta *interface2 = new InterfacePorta("Porta1-CAD1", "CAD1");
+    InterfacePorta *interface3 = new InterfacePorta("Porta1-Sala11", "Sala11");
 
     if (interface1->abrirPorta(606060)) {
         cout << ">>>>>>>>>.................................................<<<< ACESSO LIBERADO >>>>." << "\n";
     } else {
         cout << ">>>>>>>>>.................................................<<<< ACESSO NEGADO >>>>." << "\n";
+    }   
+
+    if (interface2->abrirPorta(606060)) {
+        cout << ">>>>>>>>>.................................................<<<< ACESSO LIBERADO >>>>." << "\n";
+    } else {
+        cout << ">>>>>>>>>.................................................<<<< ACESSO NEGADO >>>>." << "\n";
     }
 
-    controlador.addUsuario("Bruno", 505050);
+    if (interface3->abrirPorta(808080)) {
+        cout << ">>>>>>>>>.................................................<<<< ACESSO LIBERADO >>>>." << "\n";
+    } else {
+        cout << ">>>>>>>>>.................................................<<<< ACESSO NEGADO >>>>." << "\n";
+    }   
 }
